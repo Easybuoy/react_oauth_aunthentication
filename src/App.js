49ef363ee37214da0c49b00f045c61d0f5087458
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-12-19 12:23:56 +0100

    install google client application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import { GoogleLogin } from 'react-google-login';
 
 function App() {
+  const responseGoogle = response => {
+    console.log(response);
+  };
   return (
     <div className="App">
-      
+      <GoogleLogin
+        clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
     </div>
   );
 }
